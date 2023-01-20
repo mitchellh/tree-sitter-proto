37e26047a7caa354005473094d51af4d748f32d7
Rick Winfrey <rewinfrey@github.com>
2023-01-19 16:02:11 -0800

    Add tag queries for symbol extraction

---
+++ queries/tags.scm
@@ -0,0 +1,9 @@
+(enum (enum_name) @name) @definition.class
+
+(message (message_name) @name) @definition.class
+
+(service (service_name) @name) @definition.interface
+
+(rpc (rpc_name) @name) @definition.function
+
+(field (identifier) @name) @definition.field
